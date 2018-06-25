36857301445dc4c197ce8c077eebacdc907654d7
xn012557 <lichunlin@xiaoniu66.com>
2018-06-26 00:31:26 +0800

    'ch02'

---
+++ src/main/java/com/study/netty/ch02/EchoServer.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017 xiaoniu, Inc. All rights reserved.
+ *
+ * @author chunlin.li
+ *
+ */
+package com.study.netty.ch02;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+
+/**
+ * 功能描述: XXXX
+ * <p/>
+ * 创建人: chunlin.li
+ * <p/>
+ * 创建时间: 2018/06/26.
+ * <p/>
+ * Copyright (c) 凌霄阁工作室-版权所有
+ */
+public class EchoServer {
+    private final int port;
+
+    public EchoServer (int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: "+ EchoServer.class.getSimpleName() +" <port>");
+        }
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port);
+    }
+
+    public void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                   /* .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            // TODO
+                        }
+                    })*/;
+
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+
+}
